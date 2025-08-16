6bccef9e4b4991b7ef95c7fc7a30af4fac308ae8
Vinayak Arora <vinayakarora7461@gmail.com>
2025-08-17 01:24:34 +0530

    feat: implement session check and redirect in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,23 @@
 import { useEffect } from "react";
+
 import { useUser } from "@auth0/nextjs-auth0"; // make sure you import from /client
 import { Typography, Button } from "antd";
 import Image from "next/image";
 import Loading from "@/components/loading";
-const { Text } = Typography;
 import { gql } from "graphql-tag";
 import { useQuery } from "@apollo/client";
 import apolloClient from "@/lib/apollo-client";
 import { GetServerSidePropsContext } from "next";
 import { auth0 } from "@/lib/auth0";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await auth0.getSession();
   const user = session?.user;
 
-  if (!user) {
-    return "lol";
-  }
+  // if (!user) {
+  //   return "lol";
+  // }
 
   // if (isLoading) {
   //   return (
@@ -25,6 +26,10 @@ export default async function Home() {
   //     </main>
   //   );
   // }
+
+  if (!session) {
+    return redirect("/auth/login");
+  }
 
   return (
     <main className="p-8 flex items-center justify-center min-h-screen">
@@ -37,15 +42,10 @@ export default async function Home() {
         priority
       />
       <div className="flex flex-col items-center gap-4">
-        <Text>Welcome, {user?.name}!</Text>
-        <a href="/auth/login">
-          <Button type="default">
-            <Text>Login</Text>
-          </Button>
-        </a>
+        Welcome, {user?.name}!{" "}
         <a href="/auth/logout">
           <Button type="default" danger>
-            <Text>Logout</Text>
+            Logout
           </Button>
         </a>
         <pre className="mt-8 bg-gray-100 p-4 rounded-lg overflow-x-auto">
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { auth0 } from "./lib/auth0";
 
+console.log(process.env);
+
 export async function middleware(request: NextRequest): Promise<NextResponse> {
   const session = await auth0.getSession(request);
   // console.log({ session });
